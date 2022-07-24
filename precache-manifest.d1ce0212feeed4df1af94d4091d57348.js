5834f46197be33eccaf21a3cd161da338a1f2e99
Brandon Chaimiti <branchai.1234@gmail.com>
2022-07-23 20:45:51 -0400

    Updates

---
+++ precache-manifest.d1ce0212feeed4df1af94d4091d57348.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e7c33afe6bee4af8555",
+    "url": "/Marvel_API/static/css/main.11d14f22.chunk.css"
+  },
+  {
+    "revision": "3e7c33afe6bee4af8555",
+    "url": "/Marvel_API/static/js/main.3e7c33af.chunk.js"
+  },
+  {
+    "revision": "e0035ff209ab13bd09af",
+    "url": "/Marvel_API/static/js/1.e0035ff2.chunk.js"
+  },
+  {
+    "revision": "aa692775ef283291ff09",
+    "url": "/Marvel_API/static/js/runtime~main.aa692775.js"
+  },
+  {
+    "revision": "8a7e615817bb98b168e401e76018fd9e",
+    "url": "/Marvel_API/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/FirstWebsite/precache-manifest.352a0ddb219c55f4567583403e5108e9.js"
+  "/Marvel_API/precache-manifest.d1ce0212feeed4df1af94d4091d57348.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/FirstWebsite/index.html", {
+workbox.routing.registerNavigationRoute("/Marvel_API/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
